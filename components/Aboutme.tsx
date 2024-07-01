21a46243cc3ace028010ffcc0daa2fb8fac2a6bd
Abdulbosit <acunnatov@gmail.com>
2024-07-01 14:56:20 +0500

    changes commit

--- components/Aboutme.tsx
+++ components/Aboutme.tsx
@@ -1,10 +1,58 @@
 import { SiInformatica } from "react-icons/si";
 import Title from "./Title";
 import { useLanguage } from "../context/LanguageContext"; // Assuming you have a LanguageContext
-import translations  from "../translations"; // Check the correct path to translations file
 
 const Aboutme = () => {
   const { language } = useLanguage();
+  type Translations = {
+    [key: string]: {
+      aboutme: {
+        heading: string;
+        content: string;
+      };
+      contact: {
+        heading: string;
+        description: string;
+        button: string;
+      };
+    };
+  };
+  const translations :Translations = {
+    
+    en: {
+      aboutme: {
+        heading: "About Me",
+        content: "This is content in English.",
+      },
+      contact: {
+        heading: "Contact",
+        description: "This is contact description in English.",
+        button: "Contact Me",
+      },
+    },
+    uz: {
+      aboutme: {
+        heading: "Men Haqimda",
+        content: "Bu O'zbek tilida mazmun.",
+      },
+      contact: {
+        heading: "Bog'lanish",
+        description: "Bu bog'lanish ma'lumotlari O'zbek tilida.",
+        button: "Menga Murojaat Qiling",
+      },
+    },
+    ru: {
+      aboutme: {
+        heading: "Обо мне",
+        content: "Это содержание на русском.",
+      },
+      contact: {
+        heading: "Контакт",
+        description: "Это контактная информация на русском.",
+        button: "Свяжитесь со мной",
+      },
+    },
+  } 
 
   // Ensure translations object is correctly imported and accessed
   const { heading, content } = translations[language as keyof typeof translations]?.aboutme || {};
